554edcc4e28accf5d19aa45bef4f970cf3a99496
jackie <dawao@163.com>
2017-04-20 15:18:25 +0800

    login and Echarts

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,44 +1,22 @@
 import React, { Component } from 'react'
-import {
-  Text,
-  View,
-  TouchableOpacity,
-  StyleSheet
-} from 'react-native'
 import { observer, inject } from 'mobx-react/native'
 // import { account } from '../stores';
+import LoginView from '../components/LoginView'
 
 // this is a traditional React component connected wrapped in an observer function
 @inject('account') @observer
 export default class LoginScreen extends Component {
   render () {
     return (
-      <View style={{flex: 1, padding: 20}}>
-
-        <TouchableOpacity onPress={this.onLoginPress.bind(this)}>
-          <Text style={styles.button}>Login</Text>
-        </TouchableOpacity>
-      </View>
+      <LoginView onPress={this.onLoginPress.bind(this)} onSignupPress={this.displaySignupView.bind(this)} />
     )
   }
 
   onLoginPress () {
     this.props.account.login()
   }
-}
 
-const styles = StyleSheet.create({
-  text: {
-    textAlign: 'center',
-    fontSize: 18,
-    marginBottom: 10,
-    marginTop: 10
-  },
-  button: {
-    textAlign: 'center',
-    fontSize: 18,
-    marginBottom: 10,
-    marginTop: 10,
-    color: 'blue'
+  displaySignupView () {
+    console.log('signup')
   }
-})
+}
--- src/screens/SimpleTabs.js
+++ src/screens/SimpleTabs.js
@@ -14,6 +14,7 @@ import {
 import { inject } from 'mobx-react/native'
 import Ionicons from 'react-native-vector-icons/Ionicons'
 import React from 'react'
+import Echarts from 'native-echarts'
 
 import SampleText from '../components/SampleText'
 import TodoList from './TodoList'
@@ -55,10 +56,29 @@ MyHomeScreen.navigationOptions = {
   }
 }
 
+const option = {
+  title: {
+    text: 'ECharts 示例'
+  },
+  tooltip: {},
+  legend: {
+    data: ['销量']
+  },
+  xAxis: {
+    data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+  },
+  yAxis: {},
+  series: [{
+    name: '销量',
+    type: 'bar',
+    data: [5, 20, 36, 10, 10, 20]
+  }]
+}
 const MyPeopleScreen = () => (
-  <MyNavScreen
-    banner='People Tab'
-  />
+  <ScrollView style={styles.container}>
+    <SampleText>{'Echarts banner'}</SampleText>
+    <Echarts option={option} height={300} />
+  </ScrollView>
 )
 
 MyPeopleScreen.navigationOptions = {

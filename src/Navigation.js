e4cdea5dc064c80912be104914a7a3980b750e18
jackie <dawao@163.com>
2017-04-21 14:54:18 +0800

    simple navigation

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,74 +1,42 @@
-import { BackAndroid, Platform } from 'react-native'
+import { View, Platform } from 'react-native'
 import { observer, inject } from 'mobx-react/native'
-import { StackNavigator, addNavigationHelpers } from 'react-navigation'
-import { autorun, reaction } from 'mobx'
+import { StackNavigator } from 'react-navigation'
 import React from 'react'
 // ===[ Screens ]===
 import SimpleTabs from './screens/SimpleTabs'
 import LoginScreen from './screens/LoginScreen'
 import TodoList from './screens/TodoList'
 import NewItem from './screens/NewItem'
-import stores from './stores'
 
-// const navigationConfig = {
-//   initialRouteName: 'TodoList'
-// }
-
-// const Navigation = StackNavigator({
-//   TodoList: { screen: TodoList },
-//   NewItem: { screen: NewItem }
-// }, navigationConfig)
-// =============================================
-const AppNavigator = StackNavigator(
+const UnauthenticatedNavigator = StackNavigator(
   {
     LoginScreen: { screen: LoginScreen },
     NewItem: { screen: NewItem },
-    TodoList: { screen: TodoList },
-    Index: { screen: SimpleTabs }
+    TodoList: { screen: TodoList }
   },
   {
     initialRouteName: 'LoginScreen',
-    // headerMode: 'none',
+    headerMode: 'none',
 
    /*
    * Use modal on iOS because the card mode comes from the right,
    * which conflicts with the drawer example gesture
    */
     mode: Platform.OS === 'ios' ? 'modal' : 'card'
-  },
+  }
 )
 
-reaction(() => stores.account.token,
-  () => stores.navigationStore.navigate('Index'))
-stores.navigationStore.setNavigator(AppNavigator)
-
-@inject('navigationStore')
+@inject('account')
 @observer
 class Navigation extends React.Component {
-  componentDidMount () {
-    this.subs = BackAndroid.addEventListener('backPress', () =>
-      this.props.navigationStore.goBack())
-  }
-  componentWillUnmount () {
-    this.subs && this.subs.remove()
-  }
-  subs: ?{
-    remove: () => void,
-  } = null;
-
   render () {
-    const { navigationStore } = this.props
-    const navigation = addNavigationHelpers({
-      dispatch: navigationStore.dispatchNavigation,
-      state: navigationStore.navigationState
-    })
-    return <AppNavigator navigation={navigation} />
+    return (
+      <View style={{flex: 1}}>
+        {this.props.account.isAuthenticated ? <SimpleTabs />
+          : <UnauthenticatedNavigator />}
+      </View>
+    )
   }
 }
-
-autorun('NavigationStore State', () => {
-  console.log('navigationState', stores.navigationStore.navigationState)
-  console.log('current state', stores.navigationStore.state)
-})
 
 export default Navigation
--- src/stores/account.js
+++ src/stores/account.js
@@ -8,43 +8,45 @@ const fetch = global.fetch
  * @class Account
  */
 class Account {
-    @observable username = undefined;
-    @observable token = undefined;
-    // @observable list = []
+  @observable isAuthenticated = false;
+  @observable username = undefined;
+  @observable token = undefined;
+  // @observable list = []
 
   isLoggedIn () {
     return this.username && this.username.length > 0
   }
 
-    @action login (params) {
-      var test = new RC4('jQuery')
-      fetch(host + '/jeesite/a/mobile', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({name: test.encrypt('sysadmin'), password: test.encrypt('admin')})
-      }).then((response) => response.json())
-        .then(account => {
-          this.token = account.token
-        }).catch((error) => {
-          console.log(error)
-        })
-    }
+  @action login (params) {
+    var test = new RC4('jQuery')
+    fetch(host + '/jeesite/a/mobile', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({name: test.encrypt('sysadmin'), password: test.encrypt('admin')})
+    }).then((response) => response.json())
+      .then(account => {
+        this.token = account.token
+        this.isAuthenticated = true
+      }).catch((error) => {
+        console.log(error)
+      })
+  }
 
   logout () {
     fetch('api/account/logout')
-                   .then(() => {
-                     this.username = null
-                     this.token = null
-                   })
+    .then(() => {
+      this.username = null
+      this.token = null
+    })
   }
 
   register (params) {
     fetch('api/account/register', params)
-                   .then(account => {
-                     extendObservable(this, account)
-                   })
+    .then(account => {
+      extendObservable(this, account)
+    })
   }
 }
 

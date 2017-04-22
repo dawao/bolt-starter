6c3b96d160a2f38f894d3890adc7bdd98fad8bcd
jackie <dawao@163.com>
2017-04-22 10:12:59 +0800

    fix icon issue in some android version

--- src/screens/SimpleTabs.js
+++ src/screens/SimpleTabs.js
@@ -14,48 +14,65 @@ import TodoList from './TodoList'
 import Launch from './Launch'
 import Settings from './Settings'
 
+React.isValidElement(Ionicons)
+
+const homeIcon = {
+  icon: ({ tintColor, focused }) => (
+    <Ionicons
+      name={focused ? 'ios-home' : 'ios-home-outline'}
+      size={26}
+      style={{ color: tintColor }}
+    />
+  )
+}
+var homeNavOpt = {
+  tabBarLabel: 'Home',
+  tabBarIcon: homeIcon.icon,
+  title: '⚡ Bolt List'
+}
+// 有些android要这样才显示图标，在ios上这样会报错
+if (Platform.OS === 'android') {
+  homeNavOpt.tabBar = {
+    label: 'Home',
+    icon: homeIcon.icon
+  }
+}
+var launchNavOpt = {
+  tabBarLabel: 'People',
+  tabBarIcon: ({ tintColor, focused }) => (
+    <Ionicons
+      name={focused ? 'ios-people' : 'ios-people-outline'}
+      size={26}
+      style={{ color: tintColor }}
+    />
+  )
+}
+var settingsNavOpt = {
+  tabBarLabel: 'Settings',
+  tabBarIcon: ({ tintColor, focused }) => (
+    <Ionicons
+      name={focused ? 'ios-settings' : 'ios-settings-outline'}
+      size={26}
+      style={{ color: tintColor }}
+    />
+  )
+}
+
+// 页签设置
 const SimpleTabs = TabNavigator({
   Home: {
     screen: TodoList,
-    navigationOptions: {
-      tabBarLabel: 'Home',
-      tabBarIcon: ({ tintColor, focused }) => (
-        <Ionicons
-          name={focused ? 'ios-home' : 'ios-home-outline'}
-          size={26}
-          style={{ color: tintColor }}
-        />
-      ),
-      title: '⚡ Bolt List'
-    },
+    navigationOptions: homeNavOpt,
     path: ''
   },
   Launch: {
     screen: Launch,
-    navigationOptions: {
-      tabBarLabel: 'People',
-      tabBarIcon: ({ tintColor, focused }) => (
-        <Ionicons
-          name={focused ? 'ios-people' : 'ios-people-outline'}
-          size={26}
-          style={{ color: tintColor }}
-        />
-      )
-    },
+    navigationOptions: launchNavOpt,
     path: 'chart'
   },
   Settings: {
     screen: Settings,
-    navigationOptions: {
-      tabBarLabel: 'Settings',
-      tabBarIcon: ({ tintColor, focused }) => (
-        <Ionicons
-          name={focused ? 'ios-settings' : 'ios-settings-outline'}
-          size={26}
-          style={{ color: tintColor }}
-        />
-      )
-    },
+    navigationOptions: settingsNavOpt,
     path: 'set'
   }
 }, {
